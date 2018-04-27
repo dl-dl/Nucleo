4af7851b1ba4c014d6b7fe6acffa4341fc6dabaf
dl-dl <dl.toronto@outlook.com>
2018-04-27 00:37:56 -0400

    Minor fixes

--- Src/LittleFS/lfs_test.c
+++ Src/LittleFS/lfs_test.c
@@ -2,36 +2,32 @@
 #include "sd.h"
 #include <string.h>
 
-static const unsigned int BLOCK_SIZE = 512;
+static const unsigned int SD_BLOCK_SIZE = 512;
 
 ///// Block device operations //////
-static int lfs_bd_read(const struct lfs_config *c, lfs_block_t block,
+static int sdRead(const struct lfs_config *c, lfs_block_t block,
 					   lfs_off_t off, void *buffer, lfs_size_t size)
 {
-//	BlockDevice *bd = (BlockDevice *)c->context;
-//	return bd->read(buffer, (bd_addr_t)block*c->block_size + off, size);
-	return SDReadBlocks(buffer, block, size / BLOCK_SIZE) ? 0 : size;
+	//	ASSERT_DBG(0 == off);
+	//	ASSERT_DBG(0 == size % SD_BLOCK_SIZE);
+	return SDReadBlocks(buffer, block, size / SD_BLOCK_SIZE);
 }
 
-static int lfs_bd_prog(const struct lfs_config *c, lfs_block_t block,
+static int sdWrite(const struct lfs_config *c, lfs_block_t block,
 					   lfs_off_t off, const void *buffer, lfs_size_t size)
 {
-//	BlockDevice *bd = (BlockDevice *)c->context;
-//	return bd->program(buffer, (bd_addr_t)block*c->block_size + off, size);
-	return 0;
+	//	ASSERT_DBG(0 == off);
+	//	ASSERT_DBG(0 == size % SD_BLOCK_SIZE);
+	return SDWriteBlocks(buffer, block, size / SD_BLOCK_SIZE);
 }
 
-static int lfs_bd_erase(const struct lfs_config *c, lfs_block_t block)
+static int sdErase(const struct lfs_config *c, lfs_block_t block)
 {
-//	BlockDevice *bd = (BlockDevice *)c->context;
-//	return bd->erase((bd_addr_t)block*c->block_size, c->block_size);
-	return 0;
+	return SDErase(block, block);
 }
 
-static int lfs_bd_sync(const struct lfs_config *c)
+static int sdSync(const struct lfs_config *c)
 {
-//	BlockDevice *bd = (BlockDevice *)c->context;
-//	return bd->sync();
 	return 0;
 }
 
@@ -43,14 +39,14 @@ void test_lfs(void)
 	struct lfs_config cfg;
 	memset(&cfg, 0, sizeof(cfg));
 
-	cfg.context = 0;
-	cfg.read = lfs_bd_read;
-	cfg.prog = lfs_bd_prog;
-	cfg.erase = lfs_bd_erase;
-	cfg.sync = lfs_bd_sync;
-	cfg.read_size = BLOCK_SIZE;
-	cfg.prog_size = BLOCK_SIZE;
-	cfg.block_size = BLOCK_SIZE;
+	cfg.context = 0; // transparent context
+	cfg.read = sdRead;
+	cfg.prog = sdWrite;
+	cfg.erase = sdErase;
+	cfg.sync = sdSync;
+	cfg.read_size = SD_BLOCK_SIZE;
+	cfg.prog_size = SD_BLOCK_SIZE;
+	cfg.block_size = SD_BLOCK_SIZE;
 //	cfg.block_count = hbd.size() / cfg.block_size;
 //	cfg.lookahead = 32 * ((cfg.block_count + 31) / 32);
 	cfg.block_count = 32*1024*1024;
